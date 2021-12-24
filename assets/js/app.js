f1c300bb00f08181c5bb6baaf0e3243787496020
apham1594 <88216187+apham1594@users.noreply.github.com>
2021-12-23 22:49:53 -0700

    made changes

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-/* Shapes */
-var svgContainer = document.getElementById('svgContainer');
-var animItem = bodymovin.loadAnimation({
-  wrapper: svgContainer,
-  animType: 'svg',
-  loop: true,
-  animationData: JSON.parse(animationData)
-});
